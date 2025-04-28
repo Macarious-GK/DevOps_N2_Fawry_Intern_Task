97590118063548af4ec6b8814000bbb8ffffffc1
MacariousGK <m.labibebidallah@nu.edu.eg>
2025-04-28 17:46:06 +0300

    adding Regex for pattern search and adding color for the match word

--- Task1_Bash-Script-mygrep/README.md
+++ Task1_Bash-Script-mygrep/README.md
@@ -11,3 +11,20 @@
 - Optimize the Flags by `parsing using getopts`
 - Add the tool to the `/bin`
 - Finish `Readme Guide` for the tool
+
+
+
+---
+### notes
+use for =~
+use <, cat, IFS for reading file 
+use -e, -f, -s for validate file exixts
+
+
+
+### Resourses
+https://www.youtube.com/watch?v=nBH5z2H5BIY
+https://www.w3schools.com/bash/bash_functions.php
+https://dev.to/ifenna__/adding-colors-to-bash-scripts-48g4
+https://dev.to/arsenikavalchuk/match-a-string-with-regular-expression-in-bash-3125#:~:text=In%20Bash%2C%20matching%20strings%20against,checks%20directly%20in%20Bash%20scripts.
+https://kodekloud.com/blog/bash-getopts/#parsing-command-line-options-using-positional-parameters
--- Task1_Bash-Script-mygrep/mygrep.sh
+++ Task1_Bash-Script-mygrep/mygrep.sh
@@ -1,9 +1,61 @@
 #!/bin/bash
+#
 
-String=$2
+FILE=$2
 
-if [[ $String =~ ^$1 ]]; then
-  echo "Matches!"
-else
-  echo "No match!"
-fi
+RED="\e[31m"
+ENDCOLOR="\e[0m"
+
+Regex="^(.*)($1)(.*)$"
+mygrep_search() {
+  while IFS= read -r line; do
+    if [[ $line =~ $Regex ]]; then
+      # echo "Matches!"
+      Starting_Half="${BASH_REMATCH[1]}"
+      Matching_Half="${BASH_REMATCH[2]}"
+      Ending_Half="${BASH_REMATCH[3]}"
+      echo -e "$Starting_Half${RED}$Matching_Half${ENDCOLOR}$Ending_Half"
+    fi
+    # echo "$line"
+  done < "$FILE"
+}
+
+
+
+# FILE=$3
+
+# if [ -f $FILE ]; then
+#    echo "File $FILE exists."
+# else
+#    echo "File $FILE does not exist."
+# fi
+
+
+# if [[ $String =~ ^$1 ]]; then
+#   echo "Matches!"
+# else
+#   echo "No match!"
+# fi
+# case ${3,,} in 
+#   -v)
+#     echo "Inverted match"
+#     ;;
+#   -n)
+#     echo "Line number"
+#     ;;
+#   -nv | -vn)
+#     echo "Inverted match with line number"
+#     ;;   
+#   -h)
+#     echo "Help"
+#     ;;
+
+#   *)
+#     echo "Invalid option"
+#     ;;
+# esac
+
+# Argu_1=$1
+# if [$Argu_1 -eq '-v' || '-n']; then
+#   Argu_2=$2
+# fi
