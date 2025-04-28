d3cc21cf1324365889f856620ee3e9b907e3e6b9
MacariousGK <m.labibebidallah@nu.edu.eg>
2025-04-28 20:37:55 +0300

    handling error like missing file or search string and add all flags to array for process and adding parse_args function for classifing args into flags file and search string

--- Task1_Bash-Script-mygrep/mygrep.sh
+++ Task1_Bash-Script-mygrep/mygrep.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 
-
-
 check_file(){
   FILE=$1
   if [ ! -f "$FILE" ] || [ ! -s "$FILE" ]; then
-  echo "File $FILE does not exist or has no content."
+  echo "mygrep: $FILE: No such file or directory or it has no content."
   exit 1
   fi
 }
 check_missing_args() {
   Argument_mode=$1  # Get the number of arguments passed to the function
   if [ $Argument_mode -eq 0 ] || [ $Argument_mode -eq 1 ]; then
-    echo "Number of arguments provided is $Argument_mode. Please provide a search string and a file."
+    echo "This tool expect 2 or more arguments. Please provide a search string, optional flags and a file."
     echo "Usage: $0 <search_string> [options_flags] <file_path>"
+    exit 1
+  fi
+}
+check_missing_search_string() {
+  if [ -z "$search_string" ]; then
+    echo "mygrep: No search string provided."
     exit 1
   fi
 }
@@ -27,38 +31,75 @@ mygrep_simple_search() {
     fi
   done < "$FILE"
 }
+parse_args() {
+  local found_search_string=false
+  for arg in "$@"; do
+    if [[ "$arg" == -* ]]; then
+      flags+=("$arg")
+    elif [[ "$found_search_string" == false ]]; then
+      if [ -f "$arg" ]; then
+        check_file "$arg"
+      else
+        search_string="$arg"
+        found_search_string=true
+      fi
+    else
+      check_file "$arg"
+    fi
+  done
+}
 
 Argument_mode=$#
 FILE="${!#}"
+search_string=""
+flags=()
 
 check_missing_args $Argument_mode
 check_file $FILE
-
-mygrep_simple_search() {
-  while IFS= read -r line; do
-    if [[ $line =~ $Regex ]]; then
-      Starting_Half="${BASH_REMATCH[1]}"
-      Matching_Half="${BASH_REMATCH[2]}"
-      Ending_Half="${BASH_REMATCH[3]}"
-      echo -e "${Starting_Half} ${RED}$Matching_Half${ENDCOLOR}$Ending_Half"
-    fi
-  done < "$FILE"
-}
-
-pattern=$(( $# - 1 ))
-echo "Pattern Argument is $pattern"
-# Regex="^(.*)($pattern)(.*)$"
-Regex="^(.*)($1)(.*)$"
-
-RED="\e[31m"  
-ENDCOLOR="\e[0m"
+parse_args "$@"
+check_missing_search_string $search_string
 
 
 
 
 
 
-mygrep_simple_search
+
+
+echo "Search string is $search_string"
+echo "Flags are ${flags[@]}"
+echo "File is $FILE"
+
+
+# mygrep_simple_search() {
+#   while IFS= read -r line; do
+#     if [[ $line =~ $Regex ]]; then
+#       Starting_Half="${BASH_REMATCH[1]}"
+#       Matching_Half="${BASH_REMATCH[2]}"
+#       Ending_Half="${BASH_REMATCH[3]}"
+#       echo -e "${Starting_Half} ${RED}$Matching_Half${ENDCOLOR}$Ending_Half"
+#     fi
+#   done < "$FILE"
+# }
+
+# pattern=$(( $# - 1 ))
+# echo "Pattern Argument is $pattern"
+# # Regex="^(.*)($pattern)(.*)$"
+# Regex="^(.*)($1)(.*)$"
+
+# RED="\e[31m"  
+# ENDCOLOR="\e[0m"
+
+# counter=0
+# echo "Argument: $arg ${counter}"
+# counter=$((counter + 1))
+
+
+
+
+
+
+# mygrep_simple_search
 
 # FILE=$3
 
